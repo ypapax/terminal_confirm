1ad5823286fc44e11b1b43d1fa927718c61d110d
Maxim Yefremov <maxYefr@gmail.com>
2022-05-01 18:40:15 +0300

    works

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/ypapax/terminal_confirm
+
+go 1.18
+
+require (
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/sirupsen/logrus v1.8.1 // indirect
+	golang.org/x/sys v0.0.0-20220429233432-b5fbb4746d32 // indirect
+)
---
+++ terminal.go
@@ -0,0 +1,28 @@
+package helper
+
+import (
+	"fmt"
+	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
+	"strings"
+)
+
+func Ask4confirm(msg string) (bool, error) {
+	var s string
+
+	fmt.Printf(msg + " (y/N): ")
+	_, err := fmt.Scan(&s)
+	if err != nil {
+		return false, errors.WithStack(err)
+	}
+
+	s = strings.TrimSpace(s)
+	s = strings.ToLower(s)
+
+	if s == "y" || s == "yes" {
+		logrus.Infof("approved")
+		return true, nil
+	}
+	logrus.Infof("declined")
+	return false, nil
+}
